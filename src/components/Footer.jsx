04467f1260940dd7a0c2e070b2bdf3957324aa9a
mr-Ayalk <ayalkteketel7@gmail.com>
2025-08-15 11:16:51 +0300

    footer section animation updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,20 +11,21 @@ import {
 const Footer = ({ theme }) => {
   return (
     <motion.div
-     initial={{ opacity: 0, y: 50 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.8,  }}
-        viewport={{ once: true }}
-     className="bg-slate-50 dark:bg-gray-900 pt-10 sm:pt-10 mt-20 sm:mt-40 px-4 sm:px-10 lg:px-24 xl:px-40">
+      initial={{ opacity: 0, y: 50 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.8 }}
+      viewport={{ once: true }}
+      className="bg-slate-50 dark:bg-gray-900 pt-10 sm:pt-10 mt-20 sm:mt-40 px-4 sm:px-10 lg:px-24 xl:px-40"
+    >
       {/* footer top */}
       <div className="flex justify-between lg:items-center max-lg:flex-col gap-10">
-
-        <motion.div 
-         initial={{ opacity: 0, x: -30 }}
-        whileInView={{ opacity: 1, x: 0 }}
-        transition={{ duration: 0.6, delay: 0.2 }}
-        viewport={{ once: true }}
-        className="space-y-5 text-sm text-gray-700 dark:text-gray-400">
+        <motion.div
+          initial={{ opacity: 0, x: -30 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.6, delay: 0.2 }}
+          viewport={{ once: true }}
+          className="space-y-5 text-sm text-gray-700 dark:text-gray-400"
+        >
           {/* <img src="" className='w-32 sm:w-44' alt="" /> */}
           {theme === "dark" ? (
             <Logo
@@ -65,54 +66,13 @@ const Footer = ({ theme }) => {
           </ul>
         </motion.div>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        <motion.div 
-         initial={{ opacity: 0, x: -30 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.6, delay: 0.3}}
-        viewport={{ once: true }}
-        
-        className="text-gray-600 dark:text-gray-400 ">
+        <motion.div
+          initial={{ opacity: 0, x: -30 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.6, delay: 0.3 }}
+          viewport={{ once: true }}
+          className="text-gray-600 dark:text-gray-400 "
+        >
           <h3 className="font-semibold">Subscribe to our newsletter</h3>
           <p className="text-sm mt-2 mb-6">
             The latest news,articles , and resourses ,sent to your inbox weekly
@@ -133,11 +93,12 @@ const Footer = ({ theme }) => {
       <hr className="border-gray-300 dark:border-gray-600 my-6" />
       {/* footer bottom */}
       <motion.div
-       initial={{ opacity: 1, }}
+        initial={{ opacity: 1, y: -50 }}
         whileInView={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.5, delay: 0.4 }}
         viewport={{ once: true }}
-       className="pb-6 text-sm text-gray-500 flex justify-center sm:justify-between gap-4 flex-wrap">
+        className="pb-6 text-sm text-gray-500 flex justify-center sm:justify-between gap-4 flex-wrap"
+      >
         <p>Copyright 2025 &copy; GreatStack - All Right Reserved.</p>
         <div className="flex items-center justify-between gap-4">
           <FacebookIcon />
@@ -146,7 +107,7 @@ const Footer = ({ theme }) => {
           <Twitter />
         </div>
       </motion.div>
-    </div>
+    </motion.div>
   );
 };
 
