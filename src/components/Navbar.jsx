c08a434204461105727e046fd96aad5113aee4c6
mr-Ayalk <ayalkteketel7@gmail.com>
2025-08-15 09:54:09 +0300

    navbar animation completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,26 @@
 import { useState } from "react";
-
 import Logo from "./Logo.jsx";
 import { AlignLeftIcon, AlignRightIcon, ArrowRight, X } from "lucide-react";
 import ThemeToggleBtn from "./ThemeToggleBtn.jsx";
-
+import { motion } from "motion/react";
 const Navbar = ({ theme, setTheme }) => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
   return (
-    <div className="flex justify-between items-center px-4 sm:px-12 lg:px-24 xl:px-40 py-4 sticky top-0 z-20 backdrop-blur-xl font-medium bg-white/50 dark:bg-gray-900/70">
+    <motion.div
+      initial={{
+        opacity: 0,
+        y: -50,
+      }}
+      animate={{
+        opacity: 1,
+        y: 0,
+      }}
+      transition={{
+        duration: 0.6,
+        ease: "easeOut",
+      }}
+      className="flex justify-between items-center px-4 sm:px-12 lg:px-24 xl:px-40 py-4 sticky top-0 z-20 backdrop-blur-xl font-medium bg-white/50 dark:bg-gray-900/70"
+    >
       <div className="">
         {theme === "dark" ? (
           <Logo
@@ -91,7 +104,7 @@ const Navbar = ({ theme, setTheme }) => {
           </a>
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
