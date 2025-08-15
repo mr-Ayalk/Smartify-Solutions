120363869aa6103d80e5a3d0abbae8439dba2ed4
mr-Ayalk <ayalkteketel7@gmail.com>
2025-08-15 10:42:28 +0300

    team section animation completed

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,5 +1,5 @@
 import Title from "./Title";
-
+import { motion } from "motion/react";
 const teamData = [
   {
     name: "Ayalkbet Teketel",
@@ -45,7 +45,12 @@ const teamData = [
 
 const Team = () => {
   return (
-    <div className="flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-30 text-gray-800 dark:text-white">
+    <motion.div
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ once: true }}
+      className="flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-30 text-gray-800 dark:text-white"
+    >
       <Title
         title={"Meet the team"}
         desc={
@@ -54,7 +59,11 @@ const Team = () => {
       />
       <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-5">
         {teamData.map((team, index) => (
-          <div
+          <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, delay: index * 0.1 }}
+            viewport={{ once: true }}
             key={index}
             className="flex max-sm:flex-col items-center gap-5 p-4  rounded-xl border border-gray-100 dark:border-gray-700 bg-white dark:bg-gray-900 shadow-xl shadow-gray-100 dark:shadow-white/5 hover:scale-103 transition-all duration-400"
           >
@@ -63,10 +72,10 @@ const Team = () => {
               <h3 className="font-bold text-sm">{team.name}</h3>
               <p className="text-xs opacity-60">{team.title}</p>
             </div>
-          </div>
+          </motion.div>
         ))}
       </div>
-    </div>
+    </motion.div>
   );
 };
 
