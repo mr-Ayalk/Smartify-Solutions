3a1bb58215fa8de75fe38597289ff7f0b6df96fe
mr-Ayalk <ayalkteketel7@gmail.com>
2025-08-15 07:28:49 +0300

    contact section ui completed

---
+++ src/components/ContactUs.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import Title from "./Title";
+import { ArrowRight, Mail, User } from "lucide-react";
+
+const ContactUs = () => {
+  return (
+    <div
+      id="contact-us"
+      className="flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-30 text-gray-700 dark:text-white"
+    >
+      <Title
+        title={"Reach out to us"}
+        desc={
+          "From strategy to execution ,we craft digital solutions that your business forward"
+        }
+      />
+      <form className="grid sm:grid-cols-2 gap-3 sm:gap-5 max-w-2xl w-full">
+        <div className="">
+          <p className="mb-2 text-sm font-medium">Your name</p>
+          <div className="flex pl-3 rounded-lg border border-gray-300 dark:border-gray-600">
+            {/* <img src="" alt="" /> */}
+            <User className="items-center mt-3" />
+            <input
+              type="text"
+              placeholder="Enter your name"
+              className="p-3 text-sm outline-none"
+              required
+            />
+          </div>
+        </div>
+        <div className="">
+          <p className="mb-2 text-sm font-medium">Email id</p>
+          <div className="flex pl-3 rounded-lg border border-gray-300 dark:border-gray-600">
+            <Mail className="items-center mt-3" />
+            <input
+              type="text"
+              placeholder="Enter your email"
+              className="p-3 text-sm outline-none"
+            />
+          </div>
+        </div>
+        <div className=" sm:col-span-2">
+          <p className="mb-2 text-sm font-medium">Message</p>
+          <textarea
+            rows={8}
+            placeholder="Enter your message"
+            className="w-full p-3 text-sm outline-none rounded-lg border border-gray-300 dark:border-gray-600"
+          />
+        </div>
+        <button
+          type="submit"
+          className="w-max flex gap-2 bg-primary text-white text-sm px-10 py-3 rounded-full cursor-pointer hover:scale-103 transition-all"
+        >
+          Submit <ArrowRight className="w-4" />
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default ContactUs;
