2f7dd1eb673e9a22e5a58ef85b1d59bed3b271af
mr-Ayalk <ayalkteketel7@gmail.com>
2025-11-13 23:46:26 +0300

    footer updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -99,7 +99,7 @@ const Footer = ({ theme }) => {
         viewport={{ once: true }}
         className="pb-6 text-sm text-gray-500 flex justify-center sm:justify-between gap-4 flex-wrap"
       >
-        <p>Copyright 2025 &copy; GreatStack - All Right Reserved.</p>
+        <p>Copyright 2025 &copy; Ayalkbet Teketel- All Right Reserved.</p>
         <div className="flex items-center justify-between gap-4">
           <FacebookIcon />
           <LinkedinIcon />
